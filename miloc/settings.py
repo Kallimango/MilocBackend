646f6419ed38d94a4db32dc99795083004b93f72
Kallimango <karl.platzeck@posteo.de>
2025-12-19 18:32:33 +0100

    Update settings.py

--- miloc/settings.py
+++ miloc/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "milocbackend.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:38021",
     "http://127.0.0.1:8000",
-    "milocbackend.up.railway.app",
+    "https://milocbackend.up.railway.app",
 ]
 
 
