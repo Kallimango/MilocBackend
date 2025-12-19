b7b9eaaa229f7287b4b6a9aed8828bd5ce127205
Kallimango <karl.platzeck@posteo.de>
2025-12-19 18:25:31 +0100

    Update settings.py

--- miloc/settings.py
+++ miloc/settings.py
@@ -25,11 +25,12 @@ SECRET_KEY = 'secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "milocbackend.up.railway.app"]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:38021",
     "http://127.0.0.1:8000",
+    "milocbackend.up.railway.app",
 ]
 
 
