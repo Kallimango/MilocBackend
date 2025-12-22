4080a37b42008d150604dcb93de06cfe60b98b99
Kallimango <karl.platzeck@posteo.de>
2025-12-22 21:27:51 +0100

    Update wasabi.py

--- api/utils/wasabi.py
+++ api/utils/wasabi.py
@@ -1,8 +1,10 @@
 import boto3
 from django.conf import settings
+import tempfile
+from .encryption import decrypt_file
 
-def generate_signed_url(key, expires=300):
-    s3 = boto3.client(
+def get_s3_client():
+    return boto3.client(
         "s3",
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
@@ -10,6 +12,8 @@ def generate_signed_url(key, expires=300):
         region_name=settings.AWS_S3_REGION_NAME,
     )
 
+def generate_signed_url(key, expires=300):
+    s3 = get_s3_client()
     return s3.generate_presigned_url(
         "get_object",
         Params={
@@ -18,3 +22,19 @@ def generate_signed_url(key, expires=300):
         },
         ExpiresIn=expires,
     )
+
+def get_decrypted_temp_file(key, user):
+    """
+    Downloads encrypted file from S3, decrypts it locally, and returns the temp file path.
+    Caller is responsible for deleting temp file.
+    """
+    s3 = get_s3_client()
+    tmp = tempfile.NamedTemporaryFile(delete=False, suffix=key.split('.')[-1])
+    s3.download_file(settings.AWS_STORAGE_BUCKET_NAME, key, tmp.name)
+    decrypted_tmp = tempfile.NamedTemporaryFile(delete=False, suffix=key.split('.')[-1])
+    decrypt_file(tmp.name, user, decrypted_tmp.name)
+    tmp.close()
+    # remove the original encrypted temp
+    import os
+    os.remove(tmp.name)
+    return decrypted_tmp.name
