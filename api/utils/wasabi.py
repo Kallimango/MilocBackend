add74b432c7186a08bd23b5a8183ed37304bf086
Kallimango <karl.platzeck@posteo.de>
2025-12-22 20:57:15 +0100

    Create wasabi.py

---
+++ api/utils/wasabi.py
@@ -0,0 +1,20 @@
+import boto3
+from django.conf import settings
+
+def generate_signed_url(key, expires=300):
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        region_name=settings.AWS_S3_REGION_NAME,
+    )
+
+    return s3.generate_presigned_url(
+        "get_object",
+        Params={
+            "Bucket": settings.AWS_STORAGE_BUCKET_NAME,
+            "Key": key,
+        },
+        ExpiresIn=expires,
+    )
